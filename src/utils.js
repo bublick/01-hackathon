a06a6200cd46dc2e605bed37675852fc199fb4da
Kirill <k@lis.im>
2021-08-22 20:14:19 +0300

    Refactored randomGifModule

--- src/utils.js
+++ src/utils.js
@@ -1,3 +1,14 @@
 export function random(min, max) {
   return Math.round(min - 0.5 + Math.random() * (max - min + 1))
 }
+
+export function preparePlayground(){
+  if ( document.querySelector('.playground') ) document.querySelector('.playground').remove()
+
+  const playground = document.createElement('div')
+  playground.className = 'playground'
+  playground.id = 'playground'
+  document.body.append(playground)
+  
+  return playground
+}
